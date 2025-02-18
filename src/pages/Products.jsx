2d86ac8830c773929b6fcba7a514101cc4727df7
VYKUNTESWARARAO <chinnimeesala2002@gmail.com>
2025-02-18 12:26:02 +0530

    Fixed ESLint issue in Product.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,85 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Skeleton from "react-loading-skeleton";
-import { useDispatch } from "react-redux";
-import { addCart } from "../redux/action";
-import { Footer, Navbar } from "../components";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import Product from "./Product";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-
-  const addProduct = (product) => {
-    dispatch(addCart(product));
-  };
+  const componentMounted = useRef(true); // Use useRef instead of useState for componentMounted
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("https://fakestoreapi.com/products");
-        if (!response.ok) throw new Error("Failed to fetch products");
-        const data = await response.json();
-        setProducts(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
+    componentMounted.current = true;
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        if (componentMounted.current) {
+          setProducts(response.data);
+        }
+      })
+      .catch((error) => console.error(error));
 
-    fetchProducts();
+    return () => {
+      componentMounted.current = false; // Set to false when the component unmounts
+    };
   }, []);
 
-  const Loading = () => (
-    <div className="row">
-      {[...Array(4)].map((_, index) => (
-        <div key={index} className="col-md-3 mb-4">
-          <Skeleton height={300} />
-        </div>
-      ))}
-    </div>
-  );
-
-  const ShowProducts = () => (
-    <div className="row">
-      {products.map((product) => (
-        <div key={product.id} className="col-md-3 mb-4">
-          <div className="card">
-            <img
-              className="card-img-top"
-              src={product.image}
-              alt={product.title}
-              height="250px"
-            />
-            <div className="card-body">
-              <h5 className="card-title">
-                {product.title.substring(0, 15)}...
-              </h5>
-              <p className="card-text">${product.price}</p>
-              <Link to={`/product/${product.id}`} className="btn btn-dark">
-                View Details
-              </Link>
-              <button
-                className="btn btn-outline-dark mt-2"
-                onClick={() => addProduct(product)}
-              >
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-
   return (
-    <>
-      <Navbar />
-      <div className="container my-5">
-        <div className="row">{loading ? <Loading /> : <ShowProducts />}</div>
+    <div>
+      <h2>Products</h2>
+      <div className="product-list">
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 
